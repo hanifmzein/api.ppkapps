f4d6bcc1e549d43e932815c3f76a7e2d5011b362
hanifmzein <hanifmzein@gmail.com>
2020-09-19 23:53:07 +0700

    initial

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+//Route::middleware('auth:api')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+
+Route::group(['prefix' => 'v1'], function () {
+
+    Route::post('login', 'AuthController@login');
+
+    Route::get('mahasiswa', 'MahasiswaController@index');
+    Route::get('mahasiswa/{prodi_id}/{kelompok_id}', 'MahasiswaController@filter');
+    Route::put('mahasiswa/presensi/{nim}', 'PresensiController@update');
+    Route::put('mahasiswa/nilai/{nim}', 'NilaiController@update');
+    Route::get('dosen', 'DosenController@index');
+    Route::get('dosen/{nip}', 'DosenController@show');
+    Route::put('dosen/{id}', 'DosenController@update');
+    Route::get('pendamping', 'PendampingController@index');
+    Route::get('pendamping/{nim}', 'PendampingController@show');
+    Route::put('pendamping/{nim}', 'PendampingController@update');
 });
